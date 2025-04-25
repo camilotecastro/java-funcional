b1f0b9a841954899e2015389902b19fb76dbecca
CamiloCastro-tec <camilocastro.tec@gmail.com>
2025-04-25 17:28:39 -0500

    Expresiones lambda

--- src/main/java/org/example/lambdas/LambdasScope.java
+++ src/main/java/org/example/lambdas/LambdasScope.java
@@ -7,11 +7,6 @@ public class LambdasScope {
 
   public static void main(String[] args) {
 
-    // This code will not compile because the variable 'number' is effectively final
-    // and cannot be modified inside the lambda expression.
-/*    int number = 10;
-
-    IntStream.range(1, 10).forEach(i -> number = i * number);*/
 
     // Concurrent modification of a variable
     AtomicReference<Integer> number = new AtomicReference<>(10);
